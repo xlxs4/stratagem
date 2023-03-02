8a9cfe1ac9c613038328390ad96fd82339adca4c
xlxs4 <orousoultzoglou@gmail.com>
2023-03-02 14:15:15 +0200

    parser: Parser functions

--- Parser.hs
+++ Parser.hs
@@ -31,3 +31,59 @@ style = Lang.emptyDef {
 -- pattern binding using record destructuring
 Tok.TokenParser { Tok.parens = m_parens
                 , Tok.identifier = m_identifier } = Tok.makeTokenParser style
+
+-- lex input for reserved operators
+reservedOp :: T.Text -> Parser ()
+reservedOp op = Tok.reservedOp lexer $ T.unpack op
+
+-- parser functions; consume text, return LispVal data constructor
+-- with the bound value we want using monadic binding
+parseAtom :: Parser LispVal
+parseAtom = do
+  p <- m_identifier
+  return $ Atom $ T.pack p
+
+parseText :: Parser LispVal
+parseText = do
+  reservedOp "\""
+  p <- many1 $ noneOf "\""
+  reservedOp "\""
+  return $ String . T.pack $ p
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseNegNum :: Parser LispVal
+parseNegNum = do
+  char '-'
+  d <- many1 digit
+  return $ Number . negate . read $ d
+
+-- programs are a list of newline-delimited sexprs
+parseList :: Parser LispVal
+parseList = List . concat <$> Text.Parsec.many parseExpr
+                                `sepBy` (char ' ' <|> char '\n')
+
+parseSExp = List . concat <$> m_parens (Text.Parsec.many parseExpr
+                                        `sepBy` (char ' ' <|> char '\n'))
+
+parseQuote :: Parser LispVal
+parseQuote = do
+  reservedOp "\'"
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
+parseReserved :: Parser LispVal
+parseReserved = do
+  reservedOp "Nil" >> return Nil
+  <|> (reservedOp "#t" >> return (Bool True))
+  <|> (reservedOp "#f" >> return (Bool False))
+
+-- choose the first parser that can parse into a LispVal
+parseExpr :: Parser LispVal
+parseExpr = parseReserved <|> parseNumber
+  <|> try parseNegNum
+  <|> parseAtom
+  <|> parseText
+  <|> parseQuote
+  <|> parseSExp
